628f5510797bff1709ec01a1088d36c0b9087c4b
THE_G <48765068+THEG77@users.noreply.github.com>
2021-01-09 20:35:34 +0530

    Update dankmemerReact.py

--- dankmemerReact.py
+++ dankmemerReact.py
@@ -127,4 +127,12 @@ class MyClient(discord.Client):
             elif "why didn't I just go fishing" in message.content:
                 await channel.send("why didn't I just go fishing")    
 client = MyClient()
+print("""______            _   ___  ___                          ______                _             
+|  _  \          | |  |  \/  |                          | ___ \              | |            
+| | | |__ _ _ __ | | _| .  . | ___ _ __ ___   ___ _ __  | |_/ /___  __ _  ___| |_ ___  _ __ 
+| | | / _` | '_ \| |/ / |\/| |/ _ \ '_ ` _ \ / _ \ '__| |    // _ \/ _` |/ __| __/ _ \| '__|
+| |/ / (_| | | | |   <| |  | |  __/ | | | | |  __/ |    | |\ \  __/ (_| | (__| || (_) | |   
+|___/ \__,_|_| |_|_|\_\_|  |_/\___|_| |_| |_|\___|_|    \_| \_\___|\__,_|\___|\__\___/|_|   
+                                                                                            
+                                                                                            """)
 client.run("ENTER TOKEN", bot = False)
